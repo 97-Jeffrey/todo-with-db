4f5d8b6cf1712bd8ba26640957dfd77230db3741
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-12-10 15:14:11 -0800

    the post(CREATE OPERATION) is finished

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
+const pool = require('./db');
 const PORT =3000;
+
+app.use(express.json());
 
 app.get('/',(req, res)=>{
   res.send('hello wtf');
+});
+
+
+
+app.post("/todos", async (req,res)=>{
+
+  try {
+    const {description} = req.body;
+    const newTodo = await pool.query("INSERT INTO todo(description) VALUES($1) RETURNING *", [description]);
+
+    res.json(newTodo.rows[0]);
+  } catch (error) {
+    console.log(error.message)
+  }
 })
 
 app.listen(PORT, ()=>{
