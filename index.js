9ad45f2168bfdf39fce02145e29d3a6e38d01bc7
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-12-10 16:04:34 -0800

    put and delete operation are implemented

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/',(req, res)=>{
 app.get('/todos', async (req,res)=>{
 
   try {
-    const allTodos = await pool.query('SELECT * from todo');
+    const allTodos = await pool.query('SELECT * from todo ORDER BY todo_id');
     res.json(allTodos.rows);
   } catch (error) {
     console.log(error.message);
@@ -22,7 +22,7 @@ app.get('/todos', async (req,res)=>{
 app.get('/todos/:id', async (req,res)=>{
 
   try {
-    const todo = await pool.query('SELECT * from todo WHERE todo_id = ($1)', [req.params.id]);
+    const todo = await pool.query('SELECT * from todo WHERE todo_id = $1', [req.params.id]);
     res.json(todo.rows[0]);
 
   } catch (error) {
@@ -39,6 +39,32 @@ app.post("/todos", async (req,res)=>{
     const newTodo = await pool.query("INSERT INTO todo(description) VALUES($1) RETURNING *", [description]);
 
     res.json(newTodo.rows[0]);
+  } catch (error) {
+    console.log(error.message)
+  }
+})
+
+app.put('/todos/:id', async (req, res)=>{
+   try {
+     const {id} = req.params;
+     const {description} = req.body;
+
+     const updateTodo = await pool.query("UPDATE todo SET description =$1 WHERE todo_id = $2",[description, id]);
+     res.json(`todo with id of id ${id} was updated`);
+
+     
+   } catch (error) {
+     console.log(error.message)
+   }
+});
+
+
+app.delete('/todos/:id', async (req,res)=>{
+ 
+  try {
+    const {id} = req.params;
+    const deteleTodo = await pool.query('DELETE from todo WHERE todo_id =$1', [id])
+    res.json(`todo with id of ${id} was successfully deleted`);
   } catch (error) {
     console.log(error.message)
   }
