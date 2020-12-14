36494d4c71d621578927e5ea423c162b96670023
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-12-13 21:13:49 -0800

    the axios call is made to fetch data from /todo, and the all todos are rendered on the main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,42 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
+import Todo from "./todo";
+const axios = require('axios');
 
 function App(props) {
+
+  const [todos, setTodo] = useState([]);
+
+  useEffect(()=>{
+  
+    axios.get('/todos')
+      .then(res=>{
+        console.log(res);
+        setTodo(res.data);
+        
+
+      })
+
+  },[]);
+  console.log(todos)
+
+  const todoList = todos.map(todo=>{
+
+    return(
+      <Todo
+      key = {todo.todo_id}
+      description ={todo.description}
+      >
+      </Todo>
+    )
+  })
+
+
   return (
     <div className="App">
-      
+      <h1>this is my todo list</h1>
+      {todoList}
     </div>
   );
 }
---
+++ client/src/todo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Todo(props){
+
+  return(
+    <h2>
+      {props.description}
+    </h2>
+  )
+}
+
+export default Todo;
